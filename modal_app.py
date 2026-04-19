e2d58753810f5ca8be20355e8225f37d490b2bd8
johnthewebguy67-cloud <johnthewebguy67@gmail.com>
2026-04-19 17:15:55 -0400

    worker: drop to 384 resolution to fit VRAM on Modal H100
    - 50 GiB alloc at 704, 27 GiB at 512, should be ~15 GiB at 384
    - Revisit after confirming first successful render

--- modal_app.py
+++ modal_app.py
@@ -28,7 +28,7 @@ image = (
     # The echo with a commit SHA busts Modal's image cache whenever we push new code.
     # Update this SHA when you push a worker.py change and want it picked up.
     .run_commands(
-        "echo 'cache_bust_97a9b1e_512res'",
+        "echo 'cache_bust_384res'",
         "rm -rf /workspace/HunyuanVideo-Avatar",
         "git clone https://github.com/everlaunchsocial/avatar.git /workspace/HunyuanVideo-Avatar",
     )
